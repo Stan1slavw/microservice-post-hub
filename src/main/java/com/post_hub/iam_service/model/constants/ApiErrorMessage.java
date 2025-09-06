20c71841d4ed1882b039c4b4825d1555e60c4d69
Stanislav <stas1234713@gmail.com>
2025-09-06 23:55:23 +0300

    add PostController, add Get Request

---
+++ src/main/java/com/post_hub/iam_service/model/constants/ApiErrorMessage.java
@@ -0,0 +1,19 @@
+package com.post_hub.iam_service.model.constants;
+
+import lombok.Getter;
+
+@Getter
+public enum ApiErrorMessage {
+    POST_INFO_BY_ID("Receiving post with ID was not found");
+
+    private final String message;
+
+    // ЯВНЫЙ конструктор enum'а
+    ApiErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(Object... args) {
+        return String.format(message, args);
+    }
+}
