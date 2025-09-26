4e60b2e997a8ca65530a294635b7e5275c9a621b
Stanislav <stas1234713@gmail.com>
2025-09-27 00:16:42 +0300

    +add method for add new comment

--- src/main/java/com/post_hub/iam_service/controller/CommentController.java
+++ src/main/java/com/post_hub/iam_service/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.post_hub.iam_service.controller;
 
 import com.post_hub.iam_service.model.constants.ApiLogMessage;
 import com.post_hub.iam_service.model.dto.comment.CommentDTO;
+import com.post_hub.iam_service.model.request.comment.CommentRequest;
 import com.post_hub.iam_service.model.responce.IamResponse;
 import com.post_hub.iam_service.service.CommentService;
 import com.post_hub.iam_service.utils.ApiUtils;
@@ -11,10 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Validated
@@ -31,6 +29,14 @@ public class CommentController {
         log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
         IamResponse<CommentDTO> response = commentService.getCommentById(commentId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("${end.point.create}")
+    public ResponseEntity<IamResponse<CommentDTO>> createComment(@RequestBody @Valid CommentRequest commentRequest){
+        log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+
+        IamResponse<CommentDTO> response = commentService.createComment(commentRequest);
         return ResponseEntity.ok(response);
     }
 }
